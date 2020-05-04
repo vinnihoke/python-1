99ba7cbb7def82061b73eae24ee284ec02e58e3a
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-04 13:32:02 -0700

    Lists

--- src/04_printing.py
+++ src/04_printing.py
@@ -8,10 +8,12 @@ x = 10
 y = 2.24552
 z = "I like turtles!"
 
-# Using the printf operator (%), print the following feeding in the values of x,
+# ! Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-# Use the 'format' string method to print the same thing
 
-# Finally, print the same thing using an f-string
+# ! Use the 'format' string method to print the same thing
+
+# * Finally, print the same thing using an f-string
+print(f"x is {x}, y is {y}, z is {z}")
