106f412b62708a9cd02b24ca4e8f233e40aaa4d5
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-05 08:38:51 -0700

    Scopes, args partial

--- src/11_args.py
+++ src/11_args.py
@@ -1,18 +1,31 @@
 # Experiment with positional arguments, arbitrary arguments, and keyword
 # arguments.
 
-# Write a function f1 that takes two integer positional arguments and returns
+# * Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
 
+
+def f1(a, b):
+    return a + b
+
+
 print(f1(1, 2))
 
-# Write a function f2 that takes any number of integer arguments and returns the
+# * Write a function f2 that takes any number of integer arguments and returns the
 # sum.
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*args):
+    sum = 0
+    for arg in args:
+        sum += arg
+    return sum
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,21 +34,32 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
-# How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+# ! How do you have to modify the f2 call below to make this work?
+# print(f2(a))    # Should print 22
 
-# Write a function f3 that accepts either one or two arguments. If one argument,
+# * Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments.
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
 
-print(f3(1, 2))  # Should print 3
+
+def f3(*args):
+    sum = 0
+    for arg in args:
+        if len(args) == 1:
+            return arg + 1
+        else:
+            sum += arg
+    return sum
+
+
+print("Should be 3", f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
 
 
-# Write a function f4 that accepts an arbitrary number of keyword arguments and
+# ! Write a function f4 that accepts an arbitrary number of keyword arguments and
 # prints out the keys and values like so:
 #
 # key: foo, value: bar
@@ -48,18 +72,18 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
-f4(a=12, b=30)
+# f4(a=12, b=30)
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+# f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-d = {
-    "monster": "goblin",
-    "hp": 3
-}
+# d = {
+#     "monster": "goblin",
+#     "hp": 3
+# }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+# f4(d)
