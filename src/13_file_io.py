751f5fbfb7a10ad396724057220d7eb51c4386e5
Vinni Hoke <vinni.hoke@gmail.com>
2020-05-05 11:14:27 -0700

    File.io completed, starting classes

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -5,7 +5,7 @@ at how to read and write to files here:
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
-# ! Open up the "foo.txt" file (which already exists) for reading
+# * Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
@@ -15,9 +15,12 @@ with open('/Users/vinnihoke/git-projects/classes/lambda/cs/intro/python-1/src/fo
     print(read_data)
 
 
-# ! Open up a file called "bar.txt" (which doesn't exist yet) for
+# * Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('/Users/vinnihoke/git-projects/classes/lambda/cs/intro/python-1/src/bar.txt', 'w') as f:
+    sentence = "This is a line. \n This is also a line. \n This is also a line."
+    f.write(sentence)
